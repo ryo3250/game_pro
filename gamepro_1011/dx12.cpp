8c3e0c11c5d87f1a168d628cb2ea54bc8b13584a
ryo3250 <ryoman1002@icloud.com>
2025-10-25 17:09:15 +0900

    DXの宣言

---
+++ gamepro_1011/dx12.cpp
@@ -0,0 +1,10 @@
+#include "dx12.h"
+#include<d3d12.h>
+#include<dxgi1_4.h>
+#pragma comment(lib, "dxgi.lib")
+
+IDXGIFactory4* CreateDXGIFactory() 
+{
+	IDXGIFactory4* factory;
+	UNIT createFactoryFlags = 0;
+}
