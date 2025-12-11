4354e2c2348e45e8509b8b7e5ffd2b3f50cf240a
ryo3250 <ryoman1002@icloud.com>
2025-12-11 16:33:34 +0900

    fence完成

--- gamepro_1011/fence.cpp
+++ gamepro_1011/fence.cpp
@@ -1,1 +1,51 @@
 #include "fence.h"
+#include <cassert>
+
+fence::~fence()
+{
+	if (fence_) 
+	{
+		fence_->Release();
+		fence_ = nullptr;
+	}
+}
+
+[[nodiscard]] bool fence::create(const device& device) noexcept 
+{
+	HRESULT hr = device.get()->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence_));
+	if (FAILED(hr)) 
+	{
+		assert(false && "フェンスの作成に失敗しました");
+		return false;
+	}
+
+	waitGpuEvent_ = CreateEvent(nullptr, false, false, "WAIT_GPU");
+	if (!waitGpuEvent_) 
+	{
+		assert(false && "GPU同期用のイベントハンドルの作成に失敗しました");
+		return false;
+	}
+	return true;
+}
+
+void fence::wait(UINT64 fenceValue) const noexcept 
+{
+	if (!fence_) 
+	{
+		assert(false && "フェンスが未作成です");
+		return;
+	}
+
+	if (fence_->GetCompletedValue() < fenceValue) 
+	{
+		fence_->SetEventOnCompletion(fenceValue, waitGpuEvent_);
+		WaitForSingleObject(waitGpuEvent_, INFINITE);
+	}
+}
+
+[[nodiscard]] ID3D12Fence* fence::get() const noexcept {
+	if (!fence_) {
+		assert(false && "フェンスが未作成です");
+	}
+	return fence_;
+}
--- gamepro_1011/fence.h
+++ gamepro_1011/fence.h
@@ -1,5 +1,23 @@
 #pragma once
-class fence
+
+#include"device.h"
+
+class fence final
 {
+public:
+
+	fence() = default;
+
+	~fence();
+
+	[[nodiscard]] bool create(const device& device) noexcept;
+
+	void wait(UINT64 fenoeVale) const noexcept;
+
+	[[nodiscard]] ID3D12Fence* get() const noexcept;
+
+private:
+	ID3D12Fence* fence_{};
+	HANDLE       waitGpuEvent_{};
 };
 
