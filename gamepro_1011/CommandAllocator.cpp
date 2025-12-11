44f4e4fc1973b6aa865f6437e26a6f62dfda37d5
ryo3250 <ryoman1002@icloud.com>
2025-12-11 17:26:16 +0900

    CommandAllocator完成

--- gamepro_1011/CommandAllocator.cpp
+++ gamepro_1011/CommandAllocator.cpp
@@ -1,1 +1,50 @@
 #include "CommandAllocator.h"
+#include <cassert>
+
+CommandAllocator::~CommandAllocator()
+{
+	if (commandAllocator_)
+	{
+		commandAllocator_->Release();
+		commandAllocator_ = nullptr;
+	}
+}
+[[nodiscard]] bool CommandAllocator::create(const device& device, D3D12_COMMAND_LIST_TYPE type) noexcept
+{
+	HRESULT hr = device.get()->CreateCommandAllocator(type, IID_PPV_ARGS(&commandAllocator_));
+	if (FAILED(hr))
+	{
+		assert(false && "コマンドアロケーターの作成に失敗しました");
+		return false;
+	}
+	type_ = type;
+	return true;
+}
+
+void CommandAllocator::reset() noexcept 
+{
+	if (!commandAllocator_) 
+	{
+		assert(false && "コマンドアロケーターが未作成です");
+	}
+
+	commandAllocator_->Reset();
+}
+
+[[nodiscard]] ID3D12CommandAllocator* CommandAllocator::get() const noexcept
+{
+	if (!commandAllocator_)
+	{
+		assert(false && "コマンドアロケーターが未作成です");
+	}
+	return commandAllocator_;
+}
+
+[[nodiscard]] D3D12_COMMAND_LIST_TYPE CommandAllocator::getType() const noexcept
+{
+	if (!commandAllocator_)
+	{
+		assert(false && "コマンドアロケーターが未作成です");
+	}
+	return type_;
+}
--- gamepro_1011/CommandAllocator.h
+++ gamepro_1011/CommandAllocator.h
@@ -1,5 +1,23 @@
 #pragma once
+
+#include"device.h"
 class CommandAllocator
 {
+public:
+	CommandAllocator() = default;
+
+	~CommandAllocator();
+
+	[[nodiscard]] bool create(const device& device, D3D12_COMMAND_LIST_TYPE type) noexcept;
+
+	void reset() noexcept;
+
+	[[nodiscard]] ID3D12CommandAllocator* get() const noexcept;
+
+	[[nodiscard]] D3D12_COMMAND_LIST_TYPE getType() const noexcept;
+
+private:
+	ID3D12CommandAllocator* commandAllocator_{};
+	D3D12_COMMAND_LIST_TYPE type_{};
 };
 
