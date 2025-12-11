bf230084a0abe08208f00bd4699e52e955dec7a8
ryo3250 <ryoman1002@icloud.com>
2025-12-11 19:52:50 +0900

    RenderTarget途中まで完成

--- gamepro_1011/RenderTarget.cpp
+++ gamepro_1011/RenderTarget.cpp
@@ -1,1 +1,32 @@
 #include "RenderTarget.h"
+#include <cassert>
+
+RenderTarget::~RenderTarget()
+{
+	for (auto& rt : renderTragets_)
+	{
+		if (rt)
+		{
+			rt->Release();
+			rt = nullptr;
+		}
+	}
+	renderTragets_.clear();
+}
+
+[[nodiscard]] bool RenderTarget::createBackBuffer(const device& device, const swap_chain& swapChain, DescriptorHeap& heap) noexcept 
+{
+	const auto& desc = swapChain.getDesc();
+
+	renderTragets_.resize(desc.BufferCount);
+	
+	auto handle = heap.get()->GetCPUDescriptorHandleForHeapStart();
+
+	auto heapType = heap.getType();
+	assert(heapType == D3D12_DESCRIPTOR_HEAP_TYPE_RTV && "ディスクリプタヒープのタイプが RTV ではありません");
+
+	for (uint8_t i = 0; i < desc.BufferCount; ++i) 
+	{
+	
+	}
+}
--- gamepro_1011/RenderTarget.h
+++ gamepro_1011/RenderTarget.h
@@ -1,5 +1,24 @@
 #pragma once
+
+#include"device.h"
+#include"swap_chain.h"
+#include"DescriptorHeap.h"
+#include<vector>
+
 class RenderTarget
 {
+public:
+	RenderTarget() = default;
+
+	~RenderTarget();
+
+	[[nodiscard]] bool createBackBuffer(const device& device, const swap_chain& swapChain, DescriptorHeap& heap) noexcept;
+
+	[[nodiscard]] D3D12_CPU_DESCRIPTOR_HANDLE getDescriptorHandle(const device& device, const DescriptorHeap& heap, UINT index) const noexcept;
+
+	[[nodiscard]] ID3D12Resource* get(uint32_t index) const noexcept;
+
+private:
+	std::vector<ID3D12Resource*> renderTragets_;
 };
 
