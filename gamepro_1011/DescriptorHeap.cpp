38e7a2ec4ed056e100edb77e2682019934fcfe27
ryo3250 <ryoman1002@icloud.com>
2025-12-11 17:14:58 +0900

    DescriptorHeap完成

--- gamepro_1011/DescriptorHeap.cpp
+++ gamepro_1011/DescriptorHeap.cpp
@@ -14,10 +14,36 @@ DescriptorHeap :: ~DescriptorHeap()
 [[nodiscard]] bool DescriptorHeap::create(const device& device, D3D12_DESCRIPTOR_HEAP_TYPE type, UINT numDescriptors, bool shaderVisible) noexcept
 {
 	D3D12_DESCRIPTOR_HEAP_DESC HeapDesc = {};
-	rtvHeapDesc.NumDescriptors = numDescriptors; // スワップチェーンのバッファ数
-		rtvHeapDesc.Type = type;
-		rtvHeapDesc.Flags = shaderVisible ? D3D12_DESCRIPTOR_HEAR_FLAG_SHDER_VISIBLE : D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+	HeapDesc.NumDescriptors = numDescriptors; // スワップチェーンのバッファ数
+	HeapDesc.Type = type;
+	HeapDesc.Flags = shaderVisible ? D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE : D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
 
-	ID3D12DescriptorHeap* rtvHeap;
-	device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&rtvHeap));
+	type = type;
+
+	HRESULT hr = device.get()->CreateDescriptorHeap(&HeapDesc, IID_PPV_ARGS(&heap_));
+	if (FAILED(hr))
+	{
+		assert(false && "デスクリプタヒープの作成に失敗しました");
+		return false;
+	}
+	return true;
+}
+
+[[nodiscard]] ID3D12DescriptorHeap* DescriptorHeap::get() const noexcept
+{
+	if (heap_) 
+	{
+		assert(false && "デスクリプタヒープが未作成です");
+	}
+
+	return heap_;
+}
+
+[[nodiscard]] D3D12_DESCRIPTOR_HEAP_TYPE DescriptorHeap::getType() const noexcept 
+{
+	if (!heap_)
+	{
+		assert(false && "デスクリプタヒープが未作成です");
+	}
+	return type_;
 }
