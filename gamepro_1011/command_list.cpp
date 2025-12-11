05298077cf2ece3be4da6fc35dc1d4080c354c82
ryo3250 <ryoman1002@icloud.com>
2025-12-11 17:47:06 +0900

    command_list完成

--- gamepro_1011/command_list.cpp
+++ gamepro_1011/command_list.cpp
@@ -1,1 +1,45 @@
 #include "command_list.h"
+#include <cassert>
+
+command_list::~command_list()
+{
+	if (commandList_)
+	{
+		commandList_->Release();
+		commandList_ = nullptr;
+	}
+}
+
+[[nodiscard]] bool command_list::create(const device& device, const CommandAllocator& commandAllocator) noexcept 
+{
+	const auto hr = device.get()->CreateCommandList(0, commandAllocator.getType(), commandAllocator.get(),nullptr,IID_PPV_ARGS(&commandList_));
+	if (FAILED(hr))
+	{
+		assert(false && "コマンドリストの作成に失敗しました");
+		return false;
+	}
+
+	commandList_->Close();
+	return true;
+	
+}
+
+void command_list::reset(const CommandAllocator& cmmandAllocator) noexcept 
+{
+	if (!commandList_) 
+	{
+		assert(false && "コマンドリストが未作成です");
+	}
+
+	commandList_->Reset(cmmandAllocator.get(), nullptr);
+}
+
+[[nodiscard]] ID3D12GraphicsCommandList* command_list::get() const noexcept 
+{
+	if (!commandList_) 
+	{
+		assert(false && "コマンドリストが未作成です");
+	}
+	return commandList_;
+}
+
--- gamepro_1011/command_list.h
+++ gamepro_1011/command_list.h
@@ -1,5 +1,22 @@
 #pragma once
-class command_list
+
+#include "CommandAllocator.h"
+#include "device.h"
+
+class command_list final
 {
+public:
+	command_list() = default;
+
+	~command_list();
+
+	[[nodiscard]] bool create(const device& device,const CommandAllocator& commandAllocator) noexcept;
+
+	void reset(const CommandAllocator& cmmandAllocator) noexcept;
+
+	[[nodiscard]] ID3D12GraphicsCommandList* get() const noexcept;
+
+private:
+	ID3D12GraphicsCommandList* commandList_{};
 };
 
